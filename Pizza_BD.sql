4d8d4bafe02e7a5c0c66c4213f5b4e7c44b9efcb
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2025-12-19 23:15:11 +0600

    Pizza_BD

--- Pizza_BD.sql
+++ Pizza_BD.sql
@@ -5,3 +5,8 @@ select count(order_id) as total_orders from customer_orders;
 select count(distinct order_id) from customer_orders;
 
 # 3.How many successful orders were delivered by each runner?
+select runner_id, count(order_id) Succesful_Orders
+from runner_orders
+where pickup_time is not null
+group by runner_id;
+
